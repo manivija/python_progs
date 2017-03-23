2fc769f265083c0fa29646ce2a54fb5f51d37040
Vijay Manickam <vijay.manickam@ansys.com>
2017-03-23 16:31:36 -0400

    Removing obsolete files and adding one new file instead.

--- findAnyHandle.py
+++ findAnyHandle.py
@@ -1,27 +1,22 @@
 import subprocess, re, sys, os
-HANDLE_EXE = 'handle'
-
 # idea from https://gist.github.com/cwchiu/e2489a1430b420583c91
+
+HANDLE_EXE = 'handle'
+# the location of handle.exe or add handle.exe path to the path variable in the environment variables and use 'handle'
+# HANDLE_EXE = os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])),'Dependencies', 'Handle', 'handle.exe')
 
 for i,fn in enumerate(sys.argv): 
 	if i == 0:
 		continue
 
-	# print fn
 	output = subprocess.check_output([HANDLE_EXE, fn]).decode("utf-8")
 
 	# handles = re.findall('(\S+)\s+ pid:\s+(\d+)\s+type: \w+\s+([0-9A-Z]+): ', output)
-	#handles = re.findall('(\S+)\s+ pid:\s+(\d+)\s+type: \w+\s+[0-9A-Z]+:\s+(\S+)', output)
+	handles = re.findall('(\S+)\s+ pid:\s+(\d+)\s+type: \w+\s+[0-9A-Z]+:\s+(\S+)', output)
 	# handles = re.findall('(\S+)\s+ pid:\s+(\d+)', output)
-	
-	handles = []
-	for line in output.splitlines():
-		if -1 == line.find(':'):
-			continue
-		parts = line.split(maxsplit=6)
-		handles.append((parts[0], parts[2], parts[6]))
 
 	handles = set(handles)
+
 	for v in handles:
 		print('\t'.join(v))
 
