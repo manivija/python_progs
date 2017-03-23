01977659755cb93fa762074c6a84a3612cf4d4c7
Vijay Manickam <vijay.manickam@ansys.com>
2017-03-23 15:01:33 -0400

    2017_03_23_300PM: making changes to the function to run it until it finds the handle ... hit ctrl+c to exit.

--- ex_2_Original.py
+++ ex_2_Original.py
@@ -1,28 +1,27 @@
-import subprocess, re, sys, os
-HANDLE_EXE = 'handle'
-
+import subprocess, re, sys, os, time, datetime
 # idea from https://gist.github.com/cwchiu/e2489a1430b420583c91
 
-for i,fn in enumerate(sys.argv): 
-	if i == 0:
-		continue
+SEARCH_NAME = 'Roaming\Ansys\v181\SolveHandlers.xml'
 
-	# print fn
-	output = subprocess.check_output([HANDLE_EXE, fn]).decode("utf-8")
+def findOpenHandles(name):
+	output = subprocess.check_output(['handle', name]).decode('utf-8')
 
-	# handles = re.findall('(\S+)\s+ pid:\s+(\d+)\s+type: \w+\s+([0-9A-Z]+): ', output)
-	#handles = re.findall('(\S+)\s+ pid:\s+(\d+)\s+type: \w+\s+[0-9A-Z]+:\s+(\S+)', output)
-	# handles = re.findall('(\S+)\s+ pid:\s+(\d+)', output)
-	
-	handles = []
+	handles = set()
 	for line in output.splitlines():
 		if -1 == line.find(':'):
 			continue
 		parts = line.split(maxsplit=6)
-		handles.append((parts[0], parts[2], parts[6]))
+		handles.add((parts[0], parts[2], parts[6]))
 
-	handles = set(handles)
-	for v in handles:
-		print('\t'.join(v))
+	return handles
+
+while True:
+	handles = findOpenHandles(SEARCH_NAME)
+	if len(handles) != 0:
+		print(datetime.datetime.now())
+		for v in handles:
+			print('\t'.join(v))
+		break
+	time.sleep(0.1)
 
 # input('Press <Enter> to exit')
