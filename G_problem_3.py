1cf72a49e6a297414d60c3330aa66fb9189a6dbb
Vijay <manivija.m@gmail.com>
2017-03-29 02:37:18 -0400

    correct solution to problem 3

--- G_problem_3.py
+++ G_problem_3.py
@@ -1,41 +1,44 @@
-items = [1,7,8,0,-1,2,-3]
-# items = [0,0,0,0,0]
-# items = [1,7,0,-1,2,-3,-8]
-# items = [-3,-2,-4,-6,-8]
-# items = [-3,-2,-4,-5]
-# items = [3,2,4,5]
+possibleInputs = [
+	[1,7,8,0,-1,2,-3], [0,0,0,0,0], [7,0], [0,1], [0], [-2,0], [0,-2], [2,3], [-2,3],
+	[1,7,0,-1,2,-3,-8], [-3,-2,-4,-6,-8], [-3,-2,-4,-5], [3,2,4,5], [-1,-1], [-2,0,1]
+]
+
+# possibleInputs = [
+# 	[-3,-2,-4,-6,-8]
+# ]
 
 # sort items
-items = sorted(items)
-print(items)
+def answer(items):
 
-def prod(list_ent):
-	prodTotal = 1
-	for e in list_ent:
-		prodTotal *= e
-	print(prodTotal)
-	return prodTotal
+	if len(items) == 1:
+		return str(items[0])
 
-prodList = []
+	items = sorted(items)
 
-for i in range(len(items)):
-	if items[i] != 0 and len(items[0:i]):
-		print([item for item in items[0:i+1] if item != 0])
-		prodList.append(prod([item for item in items[0:i+1] if item != 0]))
+	def prod(list_ent):
+		prodTotal = 1
+		for e in list_ent:
+			prodTotal *= e
+		return prodTotal
 
-print("Here!!")
-print (prodList)
-if len(prodList):
-	if max(prodList) != prodList[len(prodList) - 1]:
-		negaveItems = [item for item in items if item < 0]
-		print(prodList[len(prodList) - 1]/negaveItems[len(negaveItems) - 1])
+	negativeList = [x for x in items if x < 0]
+	if len(negativeList) > 1:
+		if len(negativeList) % 2 != 0:
+			negativeList = negativeList[: len(negativeList) - 1 ]
 	else:
-		print(max(prodList))
-else:
-	if len(items) > len(prodList):
-		print(0)
-	else:
-		print ("error!")
+		negativeList = []
 
+	positiveList = [x for x in items if x > 0]
+
+	NetList = positiveList + negativeList
+	if len(NetList) != 0:
+		return str(prod(NetList))
+
+	return str(0)
+
+for xs in possibleInputs:
+	print(str(xs))
+	print(answer(xs))
+	print("\n")
 
 #prodList = [ prod([items[j] for j in range(i) if items[j] != 0]) for i in range(len(items)) if items[i] != 0]
